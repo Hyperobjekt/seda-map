ce6e377a200e135431d15e8e9cb925f0fe12f84f
Lane Olson <laneolson@gmail.com>
2019-02-07 09:05:44 -0800

    add env file for mapbox token, load map and set proper size

--- src/components/map-view/Map.js
+++ src/components/map-view/Map.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactMapGL from 'react-map-gl';
 
-export default function Map() {
-  return (
-    <div className="map">
-      <div className="map__container">
-        Map
+class Map extends Component {
+
+  state = {
+    viewport: {
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+
+  getContainerSize() {
+    if (!this.mapContainer) {
+      return { width: 400, height: 400 }
+    }
+    return {
+      width: this.mapContainer.clientWidth,
+      height: this.mapContainer.clientHeight
+    }
+  }
+
+  handleResize() {
+    this.updateDimensions();
+  }
+
+  updateDimensions(dimensions = this.getContainerSize()) {
+    this.setState({ 
+      viewport: { 
+        ...this.state.viewport, 
+        ...dimensions 
+      }
+    });
+  }
+  componentDidMount() {
+      window.addEventListener(
+        'resize', this.handleResize.bind(this)
+      );
+      this.updateDimensions();
+  }
+  componentWillUnmount() {
+      window.removeEventListener(
+        'resize', this.handleResize.bind(this)
+      );
+  }
+
+  render() {
+    return (
+      <div 
+        className="map"
+        ref={ (el) => this.mapContainer = el }
+      >
+        <div 
+          className="map__container"
+        >
+          <ReactMapGL
+            { ...this.state.viewport }
+            onViewportChange={ (viewport) => this.setState({ viewport }) }
+          />
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
+
+export default Map;
