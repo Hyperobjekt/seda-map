d2b65e0aba734300f0f8dcc7f96d924978b87746
Lane Olson <laneolson@gmail.com>
2019-02-07 12:15:51 -0800

    drop static data layer

--- src/style/map-style.js
+++ src/style/map-style.js
@@ -1,32 +1,6 @@
 import { fromJS } from 'immutable';
 import MAP_STYLE from './style.json';
 
-// For more information on data-driven styles, see https://www.mapbox.com/help/gl-dds-ref/
-export const dataLayer = fromJS({
-  id: 'choropleth',
-  source: 'composite',
-  'source-layer': 'counties',
-  type: 'fill',
-  interactive: true,
-  paint: {
-    'fill-color': {
-      property: 'mn_ach',
-      stops: [
-        [ 5, '#d53e4f' ],
-        [ 5, '#f46d43' ],
-        [ 7, '#fdae61' ],
-        [ 9, '#fee08b' ],
-        [ 10, '#ffffbf' ],
-        [ 12, '#e6f598' ],
-        [ 14, '#abdda4' ],
-        [ 16, '#66c2a5' ],
-        [ 18, '#3288bd' ]
-      ]
-    },
-    'fill-opacity': 0.8
-  }
-});
-
 export const getChoroplethLayer = (region, dataProp) => fromJS({
   id: 'choropleth',
   source: 'composite',
