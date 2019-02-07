c7efcd72d5b136fef1b4252b09af9ae303cbcea3
Lane Olson <laneolson@gmail.com>
2019-02-06 19:14:24 -0800

    add alt to logo

--- src/views/index.js
+++ src/views/index.js
@@ -8,7 +8,10 @@ const App = () => (
   <div className="app__container">
     <Header>
       <Link to="/">
-        <img src="https://via.placeholder.com/240x40?text=LOGO" />
+        <img 
+          alt="Logo" 
+          src="https://via.placeholder.com/240x40?text=LOGO"
+        />
       </Link>
     </Header>
     <main className="body">
