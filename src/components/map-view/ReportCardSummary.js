be55becbae986cd97783c5286b2c63154637045c
Lane Olson <laneolson@gmail.com>
2019-03-02 02:37:30 -0800

    drop unused function

--- src/components/map-view/ReportCardSummary.js
+++ src/components/map-view/ReportCardSummary.js
@@ -5,9 +5,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-const getVarLabel = (varName) => {
-
-}
 
 const ReportCardSummary = ({
   data, 
