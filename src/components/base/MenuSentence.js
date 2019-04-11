b8a0e60bac996d8cadf958cba544954ab33b1af9
Lane Olson <laneolson@gmail.com>
2019-04-11 16:03:34 -0700

    separate language into separate lang file

--- src/components/base/MenuSentence.js
+++ src/components/base/MenuSentence.js
@@ -5,9 +5,8 @@ import { Typography } from '@material-ui/core';
 
 function MenuSentence({text, controls, onChange, ...rest}) {
   let controlIndex = 0;
-  const chunks = text.split(/(\d)/)
-    .filter(chunk => chunk[chunk.length-1] === '$')
-    .map(chunk => chunk.substr(0, chunk.length-1))
+  const chunks = text.split(/(\$\{[a-zA-Z0-9_]*\})/)
+    .filter(chunk => chunk[0] !== '$' && chunk)
     .reduce((arr, chunk, i) => {
       arr.push(
         <span key={'chunk'+i+'text'}>{chunk}</span> 
@@ -31,17 +30,7 @@ function MenuSentence({text, controls, onChange, ...rest}) {
 MenuSentence.propTypes = {
   text: PropTypes.string,
   controls: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string,
-      label: PropTypes.string,
-      value: PropTypes.string,
-      options: PropTypes.arrayOf(
-        PropTypes.shape({ 
-          id: PropTypes.string, 
-          label: PropTypes.string 
-        })
-      )
-    })
+    PropTypes.shape(InlineMenu.propTypes)
   ),
   onChange: PropTypes.func
 }
