e4028af0e67a13b68fe9344563179fb01920022b
Lane Olson <laneolson@gmail.com>
2019-08-10 20:52:38 -0600

    test base route

--- src/views/index.js
+++ src/views/index.js
@@ -6,9 +6,10 @@ import withRoot from '../withRoot';
 
 const App = () => (
   <Switch>
-    <Route exact path={`${process.env.PUBLIC_URL}/`} render={() => (<Redirect to="/map/us/counties/avg/ses/all/3.5/38/-97"/>)} />
-    <Route exact path={`${process.env.PUBLIC_URL}/:view/:highlightedState/:region/:metric/:secondary/:demographic/:zoom/:lat/:lon/:locations?`} component={ ExplorerView } />
-    <Route exact path={`${process.env.PUBLIC_URL}/sandbox`} component={ SandboxView } />
+    <Route exact path={`/`} render={() => (<Redirect to="/map/us/counties/avg/ses/all/3.5/38/-97"/>)} />
+    <Route exact path={`/:view/:highlightedState/:region/:metric/:secondary/:demographic/:zoom/:lat/:lon/:locations?`} component={ ExplorerView } />
+    <Route exact path={`/explorer/:view/:highlightedState/:region/:metric/:secondary/:demographic/:zoom/:lat/:lon/:locations?`} component={ ExplorerView } />
+    <Route exact path={`/sandbox`} component={ SandboxView } />
   </Switch>
 )
 
