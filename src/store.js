ce87cadc5e8a932ac6029b9171ec0e21b2761303
Lane Olson <laneolson@gmail.com>
2019-08-10 21:26:02 -0600

    drop subfolder prefix

--- src/store.js
+++ src/store.js
@@ -57,7 +57,7 @@ const gtm = GoogleTagManager();
 
 const gtmMiddleware = createMiddleware(eventsMap, gtm);
 
-export const history = createHistory({basename: process.env.REACT_APP_SUBFOLDER})
+export const history = createHistory()
 
 const initialState = {}
 const enhancers = []
