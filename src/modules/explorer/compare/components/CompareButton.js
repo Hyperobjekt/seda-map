6cf6ee5f175109b169b5b4ecd7ec67d50beb3469
Lane Olson <laneolson@gmail.com>
2021-01-06 11:28:08 -0800

    scaffolding comparison components

--- src/modules/explorer/compare/components/CompareButton.js
+++ src/modules/explorer/compare/components/CompareButton.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import useCompareDialog from '../hooks/useCompareDialog'
-import { Button } from '@material-ui/core';
-import { getLang } from '../../app/selectors/lang';
+import { Button } from '@material-ui/core'
+import { getLang } from '../../app/selectors/lang'
+import CompareDialog from './CompareDialog'
 
 /**
  * Button that opens the compare locations dialog
  */
 const CompareButton = props => {
-  const [, setDialogOpen] = useCompareDialog();
+  const [, setDialogOpen] = useCompareDialog()
 
   const handleOpenDialog = () => {
     setDialogOpen(true)
   }
   return (
-    <Button onClick={handleOpenDialog} {...props}>
-      { getLang('LOCATION_COMPARE_BUTTON') }
-    </Button>    
+    <>
+      <Button onClick={handleOpenDialog} {...props}>
+        {getLang('LOCATION_COMPARE_BUTTON')}
+      </Button>
+      <CompareDialog />
+    </>
   )
 }
 
---
+++ src/modules/explorer/compare/components/CompareTable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CompareTable = props => {
+  return <div />
+}
+
+CompareTable.propTypes = {}
+
+export default CompareTable
