50635d04b58e087830ff3b2816075939c7374380
alegzandach <alex.deutch@gmail.com>
2021-01-08 13:26:27 -0500

    wow yeah everything

--- src/modules/explorer/menu/SedaMenu.js
+++ src/modules/explorer/menu/SedaMenu.js
@@ -1,18 +1,64 @@
 import React from 'react'
-import { makeStyles, Button, Drawer } from '@material-ui/core'
+import { makeStyles, Button, Drawer, Link } from '@material-ui/core'
 import useMenuVisibility from './useMenuVisibility'
+import clsx from 'clsx'
+
+import { MENU } from '../app/constants/site.js'
 // import { useSiteStore } from '../app/hooks'
 
 const useStyles = makeStyles(theme => ({
-  root: { width: theme.app.panelWidth },
+  root: { 
+    width: theme.app.panelWidth 
+  },
   panelPaper: {
     padding: theme.spacing(3),
     width: theme.app.panelWidth
   },
   linkRoot: {},
-  linkItem: {},
-  link: {}
+  linkItem: {
+    display: 'block',
+    margin: '48px 0 48px',
+    fontFamily: theme.typography.button.fontFamily,
+    fontSize: '1.5rem',
+    lineHeight: '0.85',
+    textTransform: 'uppercase',
+    letterSpacing: '.01rem',
+    color: theme.palette.text.primary,
+    '&:hover': {
+      color: theme.palette.text.primary,
+    },
+  },
+  link: {},
 }))
+
+const links = MENU.navItems;
+
+const MenuLinks = ({
+  label,
+  links,
+  onClick,
+  classes = {},
+  ...props
+}) => (
+  <div
+    className={clsx(
+      'menu__link-collection',
+      classes.linkCollection
+    )}
+    {...props}>
+    <span className="menu__link-label">{label}</span>
+    {Boolean(links.length) &&
+      links.map((item, i) => (
+          <Link
+            className={classes.item}
+            onClick={() => onClick(item.link)}
+            aria-label={item.label}
+            href={item.url}>
+            {item.label}
+          </Link>
+      ))}
+  </div>
+)
 
 const SedaMenu = ({ ...props }) => {
   const classes = useStyles()
@@ -27,14 +73,14 @@ const SedaMenu = ({ ...props }) => {
       <div className={classes.header}>
         <Button onClick={toggleMenu}>Close</Button>
       </div>
-      {/* <LinkCollection
+      { <MenuLinks
         classes={{
           root: classes.linkRoot,
           item: classes.linkItem,
           link: classes.link
         }}
         links={links}
-      /> */}
+      /> }
     </Drawer>
   )
 }
