2f39e40c9ee1abe8bb039f81394e38e7cdac6614
alegzandach <alegzandach@users.noreply.github.com>
2021-01-12 12:21:57 -0500

    Issue368 - Explorer site menu (#405)
    * add close button svg, style links to match original, flex links, add social links to footer
    
    * add mobile breakpoints
    
    * clean up styling
    
    * replace entries to site.js and filter them out in menu
    
    * fix linting, adjust styles to match site pages
    
    Co-authored-by: Lane Olson <laneolson@gmail.com>

--- src/modules/explorer/menu/SedaMenu.js
+++ src/modules/explorer/menu/SedaMenu.js
@@ -1,22 +1,140 @@
 import React from 'react'
-import { makeStyles, Button, Drawer } from '@material-ui/core'
+import {
+  makeStyles,
+  Button,
+  Drawer,
+  Link
+} from '@material-ui/core'
 import useMenuVisibility from './useMenuVisibility'
-// import { useSiteStore } from '../app/hooks'
+import { IconButton } from '@material-ui/core'
+import CloseIcon from '@material-ui/icons/Close'
+import { FacebookIcon, TwitterIcon } from '../../icons'
+import { MENU } from '../app/constants/site.js'
+//import { useSiteStore } from '../app/hooks'
 
 const useStyles = makeStyles(theme => ({
-  root: { width: theme.app.panelWidth },
-  panelPaper: {
-    padding: theme.spacing(3),
-    width: theme.app.panelWidth
+  root: {
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center'
+    },
+    [theme.breakpoints.up('sm')]: {
+      textAlign: 'left'
+    },
+    '& .MuiIconButton-root': {
+      borderRadius: '50%'
+    }
   },
-  linkRoot: {},
-  linkItem: {},
-  link: {}
+  panelPaper: {
+    width: 320,
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  header: {
+    padding: theme.spacing(1),
+    textAlign: 'right'
+  },
+  closeIcon: {
+    fill: theme.palette.text.primary,
+    width: '44px',
+    height: '44px'
+  },
+  closeButton: {
+    position: 'absolute',
+    top: theme.spacing(2),
+    right: theme.spacing(2),
+    width: '44px',
+    height: '44px'
+  },
+  closeText: {
+    fontFamily: 'var(--alt-font)',
+    fontSize: '11.5px',
+    color: 'var(--text)',
+    position: 'absolute',
+    bottom: '-5px',
+    left: '0',
+    right: '0',
+    textAlign: 'center',
+    width: '100%',
+    letterSpacing: '0.5px',
+    textTransform: 'uppercase'
+  },
+  linkBox: {
+    padding: theme.spacing(15.5, 3, 3, 6)
+  },
+  linkItem: {
+    display: 'block',
+    marginBottom: theme.spacing(4),
+    fontFamily: theme.typography.button.fontFamily,
+    fontSize: '1.5rem',
+    lineHeight: '0.85',
+    textTransform: 'uppercase',
+    letterSpacing: '.01rem',
+    color: theme.palette.text.primary,
+    '&:hover': {
+      color: theme.palette.text.primary
+    }
+  },
+  footer: {
+    padding: theme.spacing(0, 5, 3, 5)
+  },
+  footerItem: {
+    padding: 0,
+    minWidth: 0,
+    width: 32,
+    height: 32,
+    marginRight: theme.spacing(1),
+    '& .MuiSvgIcon-root': {
+      fontSize: theme.spacing(4)
+    }
+  }
 }))
+
+const links = MENU.navItems
+const socials = MENU.socialItems
+const socialIcons = {
+  facebook: <FacebookIcon />,
+  twitter: <TwitterIcon />
+}
+
+const MenuLinks = ({ links, classes = {}, ...props }) => (
+  <div className={classes.root}>
+    {Boolean(links.length) &&
+      links.map((item, i) => (
+        <Link
+          key={i}
+          className={classes.item}
+          aria-label={item.label}
+          href={item.url}>
+          {item.label}
+        </Link>
+      ))}
+  </div>
+)
+
+const MenuFooter = ({
+  socials,
+  icons,
+  classes = {},
+  ...props
+}) => (
+  <div className={classes.root}>
+    {Boolean(socials.length) &&
+      socials.map((item, i) => (
+        <Button
+          key={i}
+          className={classes.item}
+          aria-label={item.label}
+          target="_blank"
+          href={item.url}>
+          {item.icon ? icons[item.icon] : item.label}
+        </Button>
+      ))}
+  </div>
+)
 
 const SedaMenu = ({ ...props }) => {
   const classes = useStyles()
-  // const links = useSiteStore(state => state.menu)
+  //const links = useSiteStore(state => state.menu)
   const [showMenu, toggleMenu] = useMenuVisibility()
   return (
     <Drawer
@@ -24,17 +142,35 @@ const SedaMenu = ({ ...props }) => {
       anchor="right"
       classes={{ root: classes.root, paper: classes.panelPaper }}
       {...props}>
-      <div className={classes.header}>
-        <Button onClick={toggleMenu}>Close</Button>
+      <IconButton
+        className={classes.closeButton}
+        size="small"
+        aria-label="close"
+        onClick={toggleMenu}
+        {...props}>
+        <CloseIcon className={classes.closeIcon} />
+        <span className={classes.closeText}>Close</span>
+      </IconButton>
+      <div className={classes.linkBox}>
+        {
+          <MenuLinks
+            classes={{
+              item: classes.linkItem,
+              link: classes.link
+            }}
+            links={links}
+          />
+        }
       </div>
-      {/* <LinkCollection
-        classes={{
-          root: classes.linkRoot,
-          item: classes.linkItem,
-          link: classes.link
-        }}
-        links={links}
-      /> */}
+      <div className={classes.footer}>
+        <MenuFooter
+          classes={{
+            item: classes.footerItem
+          }}
+          socials={socials}
+          icons={socialIcons}
+        />
+      </div>
     </Drawer>
   )
 }
