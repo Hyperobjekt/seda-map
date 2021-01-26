d6bb004cc761d3ae7db3cd3b0333e8d50a1ae439
Lane Olson <lane@MacBook-Air.hitronhub.home>
2021-01-26 11:45:41 -0800

    update underlay so it is only active with 5000 or fewer dots, update progressive rendering

---
+++ src/modules/explorer/scatterplot/constants.js
@@ -0,0 +1,7 @@
+
+/** Maximum number of dots for the scatterplot */
+export const MAX_DOTS = 10000
+/** Number of dots for when the underlay should be triggered */
+export const UNDERLAY_THRESHOLD = 5001
+
+export const PROGRESSIVE_THRESHOLD = 6000
