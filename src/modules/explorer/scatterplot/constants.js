2a8e1c40b767bb7206b8ea27e1453c6ec1c9cfe3
lane <laneolson@gmail.com>
2021-01-27 14:51:56 -0800

    adjust to 3000 dots for underlay

--- src/modules/explorer/scatterplot/constants.js
+++ src/modules/explorer/scatterplot/constants.js
@@ -2,6 +2,6 @@
 /** Maximum number of dots for the scatterplot */
 export const MAX_DOTS = 10000
 /** Number of dots for when the underlay should be triggered */
-export const UNDERLAY_THRESHOLD = 5001
+export const UNDERLAY_THRESHOLD = 3000
 
 export const PROGRESSIVE_THRESHOLD = 6000
